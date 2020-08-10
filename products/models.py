5cc2deb56d68ea06682487469cb7e6c8e1b6fbfb
julian190 <68065877+julian190@users.noreply.github.com>
2020-08-10 04:14:30 +0300

    Add models for brands

--- products/models.py
+++ products/models.py
@@ -4,7 +4,8 @@ from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 class Product(models.Model):
     PDRName= models.CharField(max_length=100, verbose_name="Product Name")
-    #category
+    PDRcategory = models.ForeignKey("Category",on_delete=models.CASCADE,null=True,blank=True,verbose_name="Product Category")
+    PDRBrand = models.ForeignKey("settings.Brand",on_delete=models.CASCADE,null=True,blank=True,verbose_name="Product Brand")
     PDRDesc = models.TextField(verbose_name="Product Description")
     PDRPrice = models.DecimalField(max_digits=5,decimal_places=2, verbose_name= "Product Price")
     PDRCost = models.DecimalField(max_digits=5,decimal_places=2, verbose_name= "Product Cost")
@@ -12,6 +13,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.PDRName
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _('Products')
 #images
 class ProductImage(models.Model):
     PRDID = models.ForeignKey(Product,on_delete=models.CASCADE,verbose_name="Product")
@@ -19,6 +23,9 @@ class ProductImage(models.Model):
 
     def __str__(self):
         return str(self.PRDID)
+    class Meta:
+        verbose_name = _("Product Image")
+        verbose_name_plural = _('Product Images')
 #category
 class Category(models.Model):
     CATName = models.CharField(max_length=50,verbose_name="category name")
@@ -28,24 +35,26 @@ class Category(models.Model):
 
     def __str__(self):
         return self.CATName
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _('Categories')
 
 #altrantives
 class Product_Altrnative(models.Model):
-    PALNProduct = models.ForeignKey(Product,on_delete=models.CASCADE)
-    PALNAltrnative = models.ManyToManyField(Product,related_name='Alternative_Products')
+    PALNProduct = models.ForeignKey(Product,on_delete=models.CASCADE, verbose_name="the orginal product")
+    PALNAltrnative = models.ManyToManyField(Product,related_name='Alternative_Products',verbose_name="the alternative ")
     class Meta:
         verbose_name = _("Product Alternative")
         verbose_name_plural = _('Product Alternatives')
     def __str__(self):
         return str(self.PALNProduct)
-
+#accessory
 class Product_Accessories(models.Model):
-    PACCProduct = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='mainAccessories_product')
-    PACCAltrantives = models.ManyToManyField(Product,related_name='accessories_products')
+    PACCProduct = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='mainAccessories_product',verbose_name="Product")
+    PACCAltrantives = models.ManyToManyField(Product,related_name='accessories_products',verbose_name="the altrnatives")
     class Meta:
         verbose_name = _("Product Accessory")
         verbose_name_plural = _('Product Accessories')
     def __str__(self):
         return str(self.PACCProduct)
 
-    #accessory
--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # added apps
     'products.apps.ProductsConfig',
+    'settings.apps.SettingsConfig',
 ]
 
 MIDDLEWARE = [
